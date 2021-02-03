034a10120b11abc79dff7f5c175cf1be5fb99480
Crystal Dalsania <dalsania.crystal@gmail.com>
2021-02-02 23:54:16 -0500

    Logic applied to all the HTML elements

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+/***************************************************
+SELECT ALL REQUIRED HTML ELEMENTS
+****************************************************/
+
+// select the table body
+var tableBody = d3.select("tbody");
+
+// Select the dropdown elements
+var shapeDropDown = d3.select("#shape-select");
+var countryDropDown = d3.select("#country-select");
+var stateDropDown = d3.select("#state-select");
+var cityDropDown = d3.select("#city-select");
+
+// Select the input elements
+var inputDate = d3.select("#datetime");
+var inputCity = d3.select("#city");
+var inputState = d3.select("#state");
